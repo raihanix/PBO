93c589ba2effdcf86a9f463d15f8ff11c17e0790
raihanix <114352789+raihanix@users.noreply.github.com>
2023-12-21 17:21:51 +0700

    Add files via upload

---
+++ polimorfisme.overloadingoverriding/Pegawai.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfisme.overloadingoverriding;
+
+/**
+ *
+ * @author PCL-B00-00
+ */
+class Pegawai {
+    
+}
